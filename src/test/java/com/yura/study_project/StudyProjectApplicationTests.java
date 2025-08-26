a6d5d7901e34430d880f156b2a4f3dd673096639
Artiom Kriskovets <artem1491@mail.ru>
2025-08-26 09:17:59 +0300

    123

---
+++ src/test/java/com/yura/study_project/StudyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yura.study_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
